fd785ca647d5b08b576086aab869963046582fb6
Shi Lei <shi_lei@massclouds.com>
2024-05-16 11:33:08 +0000

    fix scheduler

--- scheduler/src/lib.rs
+++ scheduler/src/lib.rs
@@ -10,8 +10,6 @@
 #![feature(const_mut_refs)]
 
 mod cfs;
-mod fifo;
-mod round_robin;
 
 #[cfg(test)]
 mod tests;
@@ -19,8 +17,6 @@ mod tests;
 extern crate alloc;
 
 pub use cfs::{CFSTask, CFScheduler};
-pub use fifo::{FifoScheduler, FifoTask};
-pub use round_robin::{RRScheduler, RRTask};
 
 /// The base scheduler trait that all schedulers should implement.
 ///
